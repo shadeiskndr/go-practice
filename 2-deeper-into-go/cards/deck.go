e4b4ea442a5751dfb904151d48434f4e43687a13
Shahathir Iskandar <shahathiriskandar42@gmail.com>
2025-07-21 16:19:14 +0800

    feat: implement deck of cards program with file I/O and shuffling
    This commit introduces enhanced functionality for the deck of cards program, including:
    
    - Creation of a new deck of cards with standard suits and values.
    - Shuffling of the deck using a random number generator seeded with the current time.
    - Saving the deck to a file and loading it back from a file.
    - Conversion of the deck to a string representation for easy printing and saving.
    - Dealing a hand of cards from the deck.

--- 2-deeper-into-go/cards/deck.go
+++ 2-deeper-into-go/cards/deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 //Create a new type of 'deck'
 //which is a slice of strings
@@ -9,7 +15,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
@@ -32,5 +38,34 @@ func (couldbeAnything deck) print() {
 
 func deal(d deck, handSize int) (deck, deck){
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ", ")
+}
+
+func (d deck) saveToFile(filename string) error{
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ", ")
+	return deck(s)
+}
+
+func (d deck) shuffle(){
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
 
--- 2-deeper-into-go/cards/main.go
+++ 2-deeper-into-go/cards/main.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 
 func main (){
+
+	deck := newDeck()
+	deck.shuffle()
+	deck.saveToFile("all_cards.txt")
+	newDeckFromFile("all_cards.txt")
+	hand, remaining := deal(deck, 5)
+	fmt.Println("=============-hand-==============")
+	fmt.Println(hand.toString())
+	hand.saveToFile("my_hand.txt")
+	fmt.Println("===========-remaining-===========")
+	fmt.Println(remaining.toString())
+	remaining.saveToFile("remaining_cards.txt")
+
 	// cards := deck{"Six of Spades,", newCard()}
 	// cards = append(cards, "King of Clubs,")
 	// fmt.Println(cards)
@@ -29,16 +42,24 @@ func main (){
 	// 	fmt.Println(newDeck()[i])
 	// }
 
-	fmt.Println("==============================")
+	// fmt.Println("==============================")
 
-	hand, remainingCards := deal(newDeck(), 5)
-	hand.print()
+	// hand, remainingCards := deal(newDeck(), 5)
+	// hand.print()
 
-	fmt.Println("------------------------------")
+	// fmt.Println("------------------------------")
 	
-	remainingCards.print()
+	// remainingCards.print()
 
-	fmt.Println("==============================")
+	// fmt.Println("==============================")
+
+	// fmt.Println(hand.toString())
+
+	// fmt.Println("------------------------------")
+
+	// fmt.Println(remainingCards.toString())
+
+	// fmt.Println("==============================")
 }
 
 func newCard() string {
